180d28bb17513f83e92c4aec633db8573ba86232
Quarzekk <quarzekk@gmail.com>
2013-06-20 11:02:34 -0500

    World now supplies a List of loaded chunks via World.getLoadedChunks()

--- src/com/gmail/quarzekk/worldstudio/world/World.java
+++ src/com/gmail/quarzekk/worldstudio/world/World.java
@@ -1,6 +1,7 @@
 package com.gmail.quarzekk.worldstudio.world;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class World {
 	
@@ -20,6 +21,14 @@ public class World {
 	public World() {
 		this.loadedChunks = new ArrayList<Chunk>();
 		this.info = WorldInformation.EMPTY;
+	}
+	
+	/**
+	 * Gets a List of all chunks currently loaded within this world.
+	 * @return A List&lt;Chunk&gt; of loaded chunks
+	 */
+	public List<Chunk> getLoadedChunks() {
+		return this.loadedChunks;
 	}
 	
 }
