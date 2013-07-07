385351551adb84281b0ea3152144fef4493dcda1
Quarzekk <quarzekk@gmail.com>
2013-07-07 11:06:59 -0500

    Mesh parser extracts mesh file header data

--- src/com/gmail/quarzekk/worldstudio/core/config/mesh/MeshFile.java
+++ src/com/gmail/quarzekk/worldstudio/core/config/mesh/MeshFile.java
@@ -6,15 +6,161 @@ import com.gmail.quarzekk.worldstudio.core.config.IConfigFile;
 
 public class MeshFile implements IConfigFile {
 	
+	/**
+	 * The {@link File} that backs this MeshFile.
+	 */
 	private File file;
 	
+	/**
+	 * The parsed block ID.
+	 */
+	private int blockId;
+	
+	/**
+	 * The parsed block data.
+	 */
+	private int blockData;
+	
+	/**
+	 * The parsed texture tile U.
+	 */
+	private int textureTileU;
+	
+	/**
+	 * The parsed texture tile V.
+	 */
+	private int textureTileV;
+	
+	/**
+	 * The parsed texture tile span U.
+	 */
+	private int textureTileSpanU;
+	
+	/**
+	 * The parsed texture tile span V.
+	 */
+	private int textureTileSpanV;
+	
+	/**
+	 * The parsed vertex data.
+	 */
+	private float[] vertexData;
+	
+	/**
+	 * The parsed texcoord data.
+	 */
+	private float[] texcoordData;
+	
+	/**
+	 * Constructs a new MeshFile on the given {@link File}.
+	 * @param file The File to back this MeshFile
+	 */
 	public MeshFile(File file) {
 		this.file = file;
+		
+		this.blockId = -1;
+		this.blockData = -1;
+		this.textureTileU = -1;
+		this.textureTileV = -1;
+		this.textureTileSpanU = -1;
+		this.textureTileSpanV = -1;
+		
+		// Max of 128 vertices per mesh
+		this.vertexData = new float[384];
+		this.texcoordData = new float[256];
 	}
 	
 	@Override
 	public File getFile() {
 		return this.file;
+	}
+	
+	/**
+	 * Gets the parsed block ID.
+	 * @return The parsed block ID
+	 */
+	public int getBlockId() {
+		return this.blockId;
+	}
+	
+	protected void setBlockId(int blockId) {
+		this.blockId = blockId;
+	}
+	
+	/**
+	 * Gets the parsed block data.
+	 * @return The parsed block data
+	 */
+	public int getBlockData() {
+		return this.blockData;
+	}
+	
+	protected void setBlockData(int blockData) {
+		this.blockData = blockData;
+	}
+	
+	/**
+	 * Gets the parsed texture tile U.
+	 * @return The parsed texture tile U
+	 */
+	public int getTextureTileU() {
+		return this.textureTileU;
+	}
+	
+	protected void setTextureTileU(int textureTileU) {
+		this.textureTileU = textureTileU;
+	}
+	
+	/**
+	 * Gets the parsed texture tile V.
+	 * @return The parsed texture tile V
+	 */
+	public int getTextureTileV() {
+		return this.textureTileV;
+	}
+	
+	protected void setTextureTileV(int textureTileV) {
+		this.textureTileV = textureTileV;
+	}
+	
+	/**
+	 * Gets the parsed texture tile span U.
+	 * @return The parsed texture tile span U
+	 */
+	public int getTextureTileSpanU() {
+		return this.textureTileSpanU;
+	}
+	
+	protected void setTextureTileSpanU(int textureTileSpanU) {
+		this.textureTileSpanU = textureTileSpanU;
+	}
+	
+	/**
+	 * Gets the parsed texture tile span V.
+	 * @return The parsed texture tile span V
+	 */
+	public int getTextureTileSpanV() {
+		return this.textureTileSpanV;
+	}
+	
+	protected void setTextureTileSpanV(int textureTileSpanV) {
+		this.textureTileSpanV = textureTileSpanV;
+	}
+	
+	/**
+	 * Gets the parsed vertex data.
+	 * @return The parsed vertex data
+	 */
+	public float[] getVertexData() {
+		return this.vertexData;
+	}
+	
+	/**
+	 * Gets the parsed texcoord data.
+	 * @return The parsed texcoord data
+	 */
+	public float[] getTexcoordData() {
+		return this.texcoordData;
 	}
 	
 }
--- src/com/gmail/quarzekk/worldstudio/core/config/mesh/MeshParser.java
+++ src/com/gmail/quarzekk/worldstudio/core/config/mesh/MeshParser.java
@@ -1,10 +1,165 @@
 package com.gmail.quarzekk.worldstudio.core.config.mesh;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import com.gmail.quarzekk.worldstudio.core.config.IConfigParser;
 
 public class MeshParser implements IConfigParser {
 	
-	public MeshParser() {
+	/**
+	 * The MeshFile parsed by this MeshParser.
+	 */
+	private MeshFile meshFile;
+	
+	/**
+	 * The current state of the parse procedure. This is used to create a
+	 * context with which subsequent lines are parsed.
+	 */
+	private int parseState;
+	
+	public MeshParser(MeshFile meshFile) {
+		this.meshFile = meshFile;
+		this.parseState = 0;
+	}
+	
+	/**
+	 * Gets the {@link MeshFile} parsed by this MeshParser.
+	 * @return The parsed MeshFile
+	 */
+	public MeshFile getMeshFile() {
+		return this.meshFile;
+	}
+	
+	/**
+	 * Parses the contents of the mesh file and stores extracted data. It is
+	 * necessary to parse the file prior to accessing any data.
+	 * @throws IOException If an I/O error occurs while reading the mesh file
+	 */
+	public void parse() throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(this.meshFile.getFile())));
+		
+		String line = null;
+		int lineNum = 0;
+		
+		while ((line = reader.readLine()) != null) {
+			lineNum++;
+			line = line.trim();
+			
+			if (line.isEmpty() || line.startsWith("#")) {
+				continue;
+			}
+			
+			this.parseLine(line, lineNum);
+		}
+		
+		reader.close();
+	}
+	
+	/**
+	 * Parses an individual line of the mesh file as provided by
+	 * {@link MeshParser#parse()}.
+	 * @param line The line to parse
+	 */
+	private void parseLine(String line, int lineNum) {
+		if (this.parseState == 0) {
+			if (line.length() >= 5 && line.substring(0, 5).equalsIgnoreCase("block")) {
+				if (line.length() >= 8 && line.substring(6, 8).equalsIgnoreCase("id")) {
+					String idString = line.substring(9);
+					int id = -1;
+					try {
+						id = Integer.parseInt(idString);
+					} catch (NumberFormatException e) {
+						this.printLineError(e, "Could not parse block ID as integer", line, lineNum, 9);
+					}
+					this.meshFile.setBlockId(id);
+				}
+				if (line.length() >= 10 && line.substring(6, 10).equalsIgnoreCase("data")) {
+					String dataString = line.substring(11);
+					int data = -1;
+					try {
+						data = Integer.parseInt(dataString);
+					} catch (NumberFormatException e) {
+						this.printLineError(e, "Could not parse block data as integer", line, lineNum, 11);
+					}
+					this.meshFile.setBlockData(data);
+				}
+			}
+			if (line.length() >= 7 && line.substring(0, 7).equalsIgnoreCase("texture")) {
+				if (line.length() >= 13 && line.substring(8, 13).equalsIgnoreCase("tileU")) {
+					String tileUString = line.substring(14);
+					int tileU = -1;
+					try {
+						tileU = Integer.parseInt(tileUString);
+					} catch (NumberFormatException e) {
+						this.printLineError(e, "Could not parse texture tile U as integer", line, lineNum, 14);
+					}
+					this.meshFile.setTextureTileU(tileU);
+				}
+				if (line.length() >= 13 && line.substring(8, 13).equalsIgnoreCase("tileV")) {
+					String tileVString = line.substring(14);
+					int tileV = -1;
+					try {
+						tileV = Integer.parseInt(tileVString);
+					} catch (NumberFormatException e) {
+						this.printLineError(e, "Could not parse texture tile V as integer", line, lineNum, 14);
+					}
+					this.meshFile.setTextureTileV(tileV);
+				}
+				if (line.length() >= 16 && line.substring(8, 17).equalsIgnoreCase("tileSpanU")) {
+					String tileSpanUString = line.substring(18);
+					int tileSpanU = -1;
+					try {
+						tileSpanU = Integer.parseInt(tileSpanUString);
+					} catch (NumberFormatException e) {
+						this.printLineError(e, "Could not parse texture tile span U as integer", line, lineNum, 18);
+					}
+					this.meshFile.setTextureTileSpanU(tileSpanU);
+				}
+				if (line.length() >= 16 && line.substring(8, 17).equalsIgnoreCase("tileSpanV")) {
+					String tileSpanVString = line.substring(18);
+					int tileSpanV = -1;
+					try {
+						tileSpanV = Integer.parseInt(tileSpanVString);
+					} catch (NumberFormatException e) {
+						this.printLineError(e, "Could not parse texture tile span V as integer", line, lineNum, 18);
+					}
+					this.meshFile.setTextureTileSpanU(tileSpanV);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Prints a formatted error string to STDERR.
+	 * @param throwable The Throwable associated with the error
+	 * @param message A message describing the error
+	 * @param line The offending line
+	 * @param lineNum The number of the line
+	 * @param charNum The number of the character within the line at which the
+	 * error occurs (-1 for the entire line)
+	 */
+	private void printLineError(Throwable throwable, String message, String line, int lineNum, int charNum) {
+		System.err.println("An error occurred while parsing mesh file \"" + this.meshFile.getFile().getAbsolutePath() + "\"");
+		System.err.println("Error: " + message);
+		System.err.println(lineNum + ":\"" + line + "\"");
+		
+		if (charNum > -1) {
+			System.err.print("+-");
+			for (int i = 0; i < String.valueOf(lineNum).length(); i++) {
+				System.err.print("-");
+			}
+			for (int i = 0; i < charNum; i++) {
+				System.err.print("-");
+			}
+			System.err.println("^");
+		}
+		
+		System.err.println();
+		throwable.printStackTrace();
+		System.err.println();
 	}
 	
 }
