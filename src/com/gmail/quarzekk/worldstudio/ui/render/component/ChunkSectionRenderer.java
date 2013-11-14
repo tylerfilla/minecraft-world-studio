f145c7984350779974afd8710a5d5908047a4290
Quarzekk <quarzekk@gmail.com>
2013-11-13 18:02:56 -0600

    Added chunk section renderer

---
+++ src/com/gmail/quarzekk/worldstudio/ui/render/component/ChunkSectionRenderer.java
@@ -0,0 +1,16 @@
+package com.gmail.quarzekk.worldstudio.ui.render.component;
+
+import java.nio.FloatBuffer;
+
+import com.gmail.quarzekk.worldstudio.world.Chunk;
+
+public class ChunkSectionRenderer {
+	
+	public ChunkSectionRenderer() {
+	}
+	
+	public int renderChunkSection(FloatBuffer buffer, Chunk chunk, int section) {
+		return 0;
+	}
+	
+}
